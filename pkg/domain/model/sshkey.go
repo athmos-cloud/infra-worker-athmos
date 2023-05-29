8f4daf14025087d9329ecc891125afd94f18e686
paulb <barrie.paul@protonmail.com>
2023-05-29 17:46:43 +0200

    fear: update makefile

---
+++ pkg/domain/model/sshkey.go
@@ -0,0 +1,7 @@
+package model
+
+type SSHKey struct {
+	KeyLength  int
+	PrivateKey string
+	PublicKey  string
+}
---
+++ pkg/usecase/repository/resource/sshkeys.go
@@ -0,0 +1,11 @@
+package resource
+
+import (
+	"github.com/athmos-cloud/infra-worker-athmos/pkg/adapter/controller/context"
+	"github.com/athmos-cloud/infra-worker-athmos/pkg/domain/model"
+	"github.com/athmos-cloud/infra-worker-athmos/pkg/kernel/errors"
+)
+
+type SSHKeys interface {
+	Create(context.Context, *model.SSHKey) errors.Error
+}
