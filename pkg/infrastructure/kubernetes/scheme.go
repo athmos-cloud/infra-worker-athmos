1b281385173a6ec4ad529914a91f8231d2149a4f
Paolo Manaois <paolo.manaois@yahoo.fr>
2023-06-06 10:14:48 +0200

    AWS scheme

--- pkg/infrastructure/kubernetes/scheme.go
+++ pkg/infrastructure/kubernetes/scheme.go
@@ -3,6 +3,9 @@ package kubernetes
 import (
 	"fmt"
 	"github.com/athmos-cloud/infra-worker-athmos/pkg/kernel/errors"
+	computeAWS "github.com/upbound/provider-aws/apis/ec2/v1beta1"
+	networkFirewallAWS "github.com/upbound/provider-aws/apis/networkfirewall/v1beta1"
+	providerAWS "github.com/upbound/provider-aws/apis/v1beta1"
 	computeGCP "github.com/upbound/provider-gcp/apis/compute/v1beta1"
 	sqlGCP "github.com/upbound/provider-gcp/apis/sql/v1beta1"
 	providerGCP "github.com/upbound/provider-gcp/apis/v1beta1"
@@ -39,5 +42,28 @@ func registerGCPResources(runtimeScheme *runtime.Scheme) {
 	UpboundSqlSchemeBuilder.Register(&sqlGCP.DatabaseInstance{}, &sqlGCP.DatabaseInstanceList{})
 	if err := UpboundSqlSchemeBuilder.AddToScheme(runtimeScheme); err != nil {
 		panic(errors.ExternalServiceError.WithMessage(fmt.Sprintf("Error registering GCP sql resources: %v", err)))
+	}
+}
+
+func registerAWSResources(runtimeScheme *runtime.Scheme) {
+	UpboundSchemeBuilder := &scheme.Builder{GroupVersion: schema.GroupVersion{Group: "aws.upbound.io", Version: "v1beta1"}}
+
+	UpboundSchemeBuilder.Register(&providerAWS.ProviderConfig{}, &providerAWS.ProviderConfigList{})
+	if err := UpboundSchemeBuilder.AddToScheme(runtimeScheme); err != nil {
+		panic(errors.ExternalServiceError.WithMessage(fmt.Sprintf("Error registering AWS resources: %v", err)))
+	}
+
+	UpboundComputeSchemeBuilder := &scheme.Builder{GroupVersion: schema.GroupVersion{Group: "ec2.aws.upbound.io", Version: "v1beta1"}}
+	UpboundComputeSchemeBuilder.Register(&computeAWS.VPC{}, &computeAWS.VPCList{})
+	UpboundComputeSchemeBuilder.Register(&computeAWS.Subnet{}, &computeAWS.SubnetList{})
+	UpboundComputeSchemeBuilder.Register(&computeAWS.Instance{}, &computeAWS.InstanceList{})
+	if err := UpboundComputeSchemeBuilder.AddToScheme(runtimeScheme); err != nil {
+		panic(errors.ExternalServiceError.WithMessage(fmt.Sprintf("Error registering AWS resources: %v", err)))
+	}
+
+	UpboundNetworkFirewallSchemeBuild := &scheme.Builder{GroupVersion: schema.GroupVersion{Group: "networkfirewall.aws.upbound.io", Version: "v1beta1"}}
+	UpboundNetworkFirewallSchemeBuild.Register(&networkFirewallAWS.RuleGroup{}, &networkFirewallAWS.RuleGroupList{})
+	if err := UpboundNetworkFirewallSchemeBuild.AddToScheme(runtimeScheme); err != nil {
+		panic(errors.ExternalServiceError.WithMessage(fmt.Sprintf("Error registering AWS resources: %v", err)))
 	}
 }
