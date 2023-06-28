eeb8db4b53e0b784ddafce5786cb6f25c3662e10
paulb <barrie.paul@protonmail.com>
2023-06-28 17:53:44 +0200

    fix: registry

--- pkg/registry/resource.go
+++ pkg/registry/resource.go
@@ -5,6 +5,7 @@ import (
 	resourceCtrl "github.com/athmos-cloud/infra-worker-athmos/pkg/adapter/controller/resource"
 	presenter "github.com/athmos-cloud/infra-worker-athmos/pkg/adapter/presenter/resource"
 	"github.com/athmos-cloud/infra-worker-athmos/pkg/adapter/repository"
+	"github.com/athmos-cloud/infra-worker-athmos/pkg/adapter/repository/aws"
 	gcpRepository "github.com/athmos-cloud/infra-worker-athmos/pkg/adapter/repository/gcp"
 	secretRepository "github.com/athmos-cloud/infra-worker-athmos/pkg/adapter/repository/secret"
 	usecase "github.com/athmos-cloud/infra-worker-athmos/pkg/usecase/usecase/resource"
@@ -14,26 +15,27 @@ func (r *registry) NewResourceController() controller.Resource {
 	projectRepo := repository.NewProjectRepository()
 	secretRepo := secretRepository.NewSecretRepository()
 	gcpRepo := gcpRepository.NewRepository()
+	awsRepo := aws.NewRepository()
 	sshKeysRepo := repository.NewSSHKeyRepository()
 
 	providerCtrl := resourceCtrl.NewProviderController(
-		usecase.NewProviderUseCase(projectRepo, secretRepo, gcpRepo, gcpRepo, gcpRepo),
+		usecase.NewProviderUseCase(projectRepo, secretRepo, gcpRepo, awsRepo, nil),
 		presenter.NewProviderPresenter(),
 	)
 	networkCtrl := resourceCtrl.NewNetworkController(
-		usecase.NewNetworkUseCase(projectRepo, gcpRepo, gcpRepo, gcpRepo),
+		usecase.NewNetworkUseCase(projectRepo, gcpRepo, awsRepo, nil),
 		presenter.NewNetworkPresenter(),
 	)
 	subnetworkCtrl := resourceCtrl.NewSubnetworkController(
-		usecase.NewSubnetworkUseCase(projectRepo, gcpRepo, gcpRepo, gcpRepo),
+		usecase.NewSubnetworkUseCase(projectRepo, gcpRepo, awsRepo, nil),
 		presenter.NewSubnetworkPresenter(),
 	)
 	firewallCtrl := resourceCtrl.NewFirewallController(
-		usecase.NewFirewallUseCase(projectRepo, gcpRepo, gcpRepo, gcpRepo),
+		usecase.NewFirewallUseCase(projectRepo, gcpRepo, awsRepo, nil),
 		presenter.NewFirewallPresenter(),
 	)
 	vmCtrl := resourceCtrl.NewVMController(
-		usecase.NewVMUseCase(projectRepo, sshKeysRepo, gcpRepo, nil, nil),
+		usecase.NewVMUseCase(projectRepo, sshKeysRepo, gcpRepo, awsRepo, nil),
 		presenter.NewVMPresenter(),
 	)
 
