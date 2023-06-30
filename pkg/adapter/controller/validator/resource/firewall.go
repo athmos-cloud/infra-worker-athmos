d65a4f7782da2170ad082fffcd134a42758b2f7b
Adem MRIZAK <adem.mrizak@gmail.com>
2023-06-30 11:27:27 +0200

    fix: Get resoruce

--- pkg/adapter/controller/validator/resource/firewall.go
+++ pkg/adapter/controller/validator/resource/firewall.go
@@ -9,19 +9,6 @@ import (
 )
 
 func GetFirewall(ctx context.Context) errors.Error {
-	if _, ok := ctx.Value(context.RequestKey).(map[string]interface{}); !ok {
-		return errors.BadRequest.WithMessage(fmt.Sprintf("expected a map got %v", ctx.Value(context.RequestKey)))
-	}
-	req := ctx.Value(context.RequestKey).(map[string]interface{})
-	jsonbody, errMarshall := json.Marshal(req)
-	if errMarshall != nil {
-		return errors.BadRequest.WithMessage(fmt.Sprintf("Invalid JSON : %v", req))
-	}
-	dtoRequest := dto.GetResourceRequest{}
-	if errUnmarshall := json.Unmarshal(jsonbody, &dtoRequest); errUnmarshall != nil {
-		return errors.BadRequest.WithMessage(fmt.Sprintf("Expected request %+v, got %v", dto.GetResourceRequest{}, req))
-	}
-	ctx.Set(context.RequestKey, dtoRequest)
 	return errors.OK
 }
 
